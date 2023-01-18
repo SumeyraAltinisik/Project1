37d75242b07715e4c993079744c8b31f9519e52b
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-18 17:29:47 +0300

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // bu kütüphane dosyasý cout ve cin komutlarýný çalýþtýrmak içindir.
+using namespace std;
+int main()
+{
+	cout << "Merhaba C++"<<endl;
+	cout << "Enstitu ismek \nIBB";
+}
